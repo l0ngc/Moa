800008c9451cc2913e193f49e7269af630f15a04
weilong-web <chenweilong505@gmail.com>
2023-05-21 19:28:16 +0200

    workflow support multiple models

---
+++ notebooks/model_dnn.py
@@ -0,0 +1,83 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+class DNN(nn.Module):
+    def __init__(self, num_features, num_targets, hidden_size):
+        super(DNN, self).__init__()
+        self.batch_norm1 = nn.BatchNorm1d(num_features)
+        self.dropout1 = nn.Dropout(0.2)
+        self.dense1 = nn.utils.weight_norm(nn.Linear(num_features, hidden_size))
+        
+        self.batch_norm2 = nn.BatchNorm1d(hidden_size)
+        self.dropout2 = nn.Dropout(0.5)
+        self.dense2 = nn.utils.weight_norm(nn.Linear(hidden_size, hidden_size))
+        
+        self.batch_norm3 = nn.BatchNorm1d(hidden_size)
+        self.dropout3 = nn.Dropout(0.5)
+        self.dense3 = nn.utils.weight_norm(nn.Linear(hidden_size, num_targets))
+    
+    def forward(self, x):
+        x = self.batch_norm1(x)
+        x = self.dropout1(x)
+        x = F.relu(self.dense1(x))
+        
+        x = self.batch_norm2(x)
+        x = self.dropout2(x)
+        x = F.relu(self.dense2(x))
+        
+        x = self.batch_norm3(x)
+        x = self.dropout3(x)
+        x = self.dense3(x)
+        
+        return x
+
+
+
+
+class AdvDNN(nn.Module):
+    def __init__(self, num_features, num_targets):
+        super(AdvDNN, self).__init__()
+        self.hidden_size = [1500, 1250, 1000, 750]
+        self.dropout_value = [0.5, 0.35, 0.3, 0.25]
+
+        self.batch_norm1 = nn.BatchNorm1d(num_features)
+        self.dense1 = nn.Linear(num_features, self.hidden_size[0])
+        
+        self.batch_norm2 = nn.BatchNorm1d(self.hidden_size[0])
+        self.dropout2 = nn.Dropout(self.dropout_value[0])
+        self.dense2 = nn.Linear(self.hidden_size[0], self.hidden_size[1])
+
+        self.batch_norm3 = nn.BatchNorm1d(self.hidden_size[1])
+        self.dropout3 = nn.Dropout(self.dropout_value[1])
+        self.dense3 = nn.Linear(self.hidden_size[1], self.hidden_size[2])
+
+        self.batch_norm4 = nn.BatchNorm1d(self.hidden_size[2])
+        self.dropout4 = nn.Dropout(self.dropout_value[2])
+        self.dense4 = nn.Linear(self.hidden_size[2], self.hidden_size[3])
+
+        self.batch_norm5 = nn.BatchNorm1d(self.hidden_size[3])
+        self.dropout5 = nn.Dropout(self.dropout_value[3])
+        self.dense5 = nn.utils.weight_norm(nn.Linear(self.hidden_size[3], num_targets))
+    
+    def forward(self, x):
+        x = self.batch_norm1(x)
+        x = F.leaky_relu(self.dense1(x))
+        
+        x = self.batch_norm2(x)
+        x = self.dropout2(x)
+        x = F.leaky_relu(self.dense2(x))
+
+        x = self.batch_norm3(x)
+        x = self.dropout3(x)
+        x = F.leaky_relu(self.dense3(x))
+
+        x = self.batch_norm4(x)
+        x = self.dropout4(x)
+        x = F.leaky_relu(self.dense4(x))
+
+        x = self.batch_norm5(x)
+        x = self.dropout5(x)
+        x = self.dense5(x)
+        return x
